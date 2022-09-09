355f8f7181f252bc2de451121b3864ad25c3b85d
ryusuke-sekido <ryu.lljd.akiyama@gmail.com>
2022-09-09 05:12:52 +0000

    削除機能追加

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -13,6 +13,12 @@ class Admin::CommentsController < ApplicationController
     @comments = Comment.all
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to admin_comments_path
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:body)
--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,18 +12,24 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def withdraw
     @customer.update(is_deleted: true)
     reset_session
     flash[:notice] = "退会処理を実行しました。"
     redirect_to root_path
   end
-  
+
+  def destroy
+    customer = Customer.find(params[:id])
+    customer.destroy
+    redirect_to admin_customers_path
+  end
+
 
   private
   def customer_params
