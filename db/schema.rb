159ab01b0198f67e225750cbc6096fc460d8eff8
ryusuke-sekido <ryu.lljd.akiyama@gmail.com>
2022-10-30 11:16:20 +0000

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -78,4 +78,9 @@ ActiveRecord::Schema.define(version: 2022_09_21_085341) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
